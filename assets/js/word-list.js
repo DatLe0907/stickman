2c3d81205866a4a027b22d964e06f41f14565728
DatLe <letandat9783@gmail.com>
2023-09-04 22:15:55 +0700

    update heart and fix UI

--- assets/js/word-list.js
+++ assets/js/word-list.js
@@ -1,7 +1,23 @@
 const wordList = [
     {
-        word: "isaac newton",
-        hint: "Ai là người phát hiện ra lực hấp dẫn? "
+        word: "hấp dẫn",
+        hint: "Mọi vật trong vũ trụ đều hút nhau với 1 lực ____"
     },
+    {
+        word: "tương đối rộng",
+        hint: "Năm 1915 Albert Einstein công bố thuyết ____"
+    },
+    {
+        word: "trọng tâm",
+        hint: "Điểm đặt của lực hấp dẫn là gì?"
+    },
+    {
+        word: "trường hấp dẫn",
+        hint: "Mô hình ____ là sự ảnh hưởng của một vật thể khối lượng lớn lên không gian bao xung quanh nó, tạo ra lực tác dụng lên một vật thể có khối lượng khác"
+    },
+    {
+        word: "ba định luật newton",
+        hint: "Ngoài phát hiện trọng lực thì newton còn nổi tiếng với lĩnh vực vật lý nào?"
+    }
     
 ];
